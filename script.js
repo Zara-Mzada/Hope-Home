bd1fff64980aadb174d696e2203db166c67e110b
Zahra Malikzada <zara@Zahras-MacBook-Air.local>
2024-06-18 13:14:12 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu-list');
+const closeButton = document.querySelector('.close');
+
+function navOpenning(){
+    menu.style.display= 'block';
+}
+function navClosing(){
+    menu.style.display = 'none';
+}
